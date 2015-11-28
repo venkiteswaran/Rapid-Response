d4ef1d9776556d511e7fe897a4411d918b56d848
varalakshmi <vara2504@gmail.com>
2015-11-28 20:51:20 +0530

    map pin db fetch

--- client/templates/RapidResponseDashboard/LiveVictimMap.js
+++ client/templates/RapidResponseDashboard/LiveVictimMap.js
@@ -1,8 +1,14 @@
+
 Template.LiveVictimMap.helpers({
   exampleMapOptions: function() {
     // Make sure the maps API has loaded
     if (GoogleMaps.loaded()) {
       // Map initialization options
+      loc = livelocation.find();
+      var lat =loc.lat;
+      var lon = loc.lon;
+     // alert(lat);
+      //alert(lon);
       return {
         center: new google.maps.LatLng(12.6, 80.1),
         zoom: 8
@@ -15,6 +21,8 @@ Template.LiveVictimMap.onCreated(function() {
   // We can use the `ready` callback to interact with the map API once the map is ready.
   GoogleMaps.ready('exampleMap', function(map) {
     // Add a marker to the map once it's ready
+   
+
     var marker = new google.maps.Marker({
       position: map.options.center,
       map: map.instance
